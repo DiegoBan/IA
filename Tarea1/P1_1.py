dcc715cca7223482182060bcc0951c29ff07e1ac
DiegoBan <bandadiego452@gmail.com>
2025-09-10 23:23:43 -0300

    Big advance T1

--- Tarea1/P1_1.py
+++ Tarea1/P1_1.py
@@ -1,9 +1,13 @@
 import pandas as pd
+from pgmpy.models import DiscreteBayesianNetwork
+from pgmpy.estimators import HillClimbSearch, BIC, BDeu, BayesianEstimator
+import logging
+logging.getLogger("pgmpy").setLevel(logging.WARNING)
 
 #   Leer csv
 dataset = pd.read_csv("dataset/vgsales.csv")
 #   Eliminar columnas que no sirven para bayes
-dataset = dataset.drop(["Rank", "Name"], axis = 1)
+dataset = dataset.drop(["Rank", "Name"], axis = 1).dropna()
 #   Discretizar datos numericos
 dataset["Year"] = pd.cut(dataset["Year"], bins=[0, 1990, 2000, 2010, 2020], 
                          labels=["<1990", "1990-2000", "2000-2010", "2010-2020"])
@@ -18,5 +22,27 @@ dataset["Other_Sales"] = pd.cut(dataset["Other_Sales"], bins=3,
 dataset["Global_Sales"] = pd.cut(dataset["Global_Sales"], bins=3,
                               labels=["Bajo", "Medio", "Alto"])
 #   Separación de datos
-dataset70 = dataset.sample(frac=0.7, random_state=42)
-dataset30 = dataset.drop(dataset70.index)
+dataset70 = dataset.sample(frac=0.7, random_state=42)   # Toma 70% de los datos aleatorio con seed 42
+dataset30 = dataset.drop(dataset70.index)               # Toma el 30% de datos restante
+print("Dataset discretizado y separado")
+print(dataset.info())
+#   Aprendizaje de estrucutra: HillClimbingSearch
+structure = HillClimbSearch(dataset70)
+model_BIC = structure.estimate(scoring_method=BIC(dataset70))
+print("\n===== Aristas Aprendidas (model1 scoring_method=BIC) =====\n", list(model_BIC.edges()))
+
+model_BDeu = structure.estimate(scoring_method=BDeu(dataset70))
+print("\n===== Aristas Aprendidas (model2 scoring_method=BDeu) =====\n", list(model_BDeu.edges()))
+
+model1 = DiscreteBayesianNetwork(model_BIC.edges())
+model2 = DiscreteBayesianNetwork(model_BDeu.edges())
+#   Estimación de parámetros:
+model1.fit(dataset70, estimator=BayesianEstimator)
+print("========== Estimaciones de model1 (scoring_method=BIC) ==========")
+for cpd in model1.get_cpds():
+    print(cpd)
+
+model2.fit(dataset70, estimator=BayesianEstimator)
+print("========== Estimaciones de model1 (scoring_method=BDeu) ==========")
+for cpd in model2.get_cpds():
+    print(cpd)
